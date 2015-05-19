e86f76c6b8449503b8790420c31e279ac96a0543
Zohaib Rauf <zabirauf@gmail.com>
2015-05-19 01:45:35 -0700

    Fixed the ethernet start

--- lib/pi_chat.ex
+++ lib/pi_chat.ex
@@ -6,7 +6,7 @@ defmodule PiChat do
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
-    if Mix.env == :prod, do: Ethernet.start
+    Ethernet.start
 
     children = [
       # Start the endpoint when the application starts
